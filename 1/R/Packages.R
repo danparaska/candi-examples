666fc38b6c865ec905dc1ffea010787976ac56a4
danparaska <dan.paraska@uwa.edu.au>
2024-03-15 16:59:50 +1100

    Simulation1

---
+++ 1/R/Packages.R
@@ -0,0 +1,21 @@
+# Install packages
+    # install.packages("MASS")
+    # install.packages("fields")
+    # install.packages("readxl")
+    # install.packages("imager")
+    # install.packages("plyr")
+    # install.packages("clipr")
+    # install.packages("readr")
+    # install.packages("data.table")
+# Load packages
+# If you have these already, then you just need to load them.
+library(data.table)
+library(plyr)
+library(MASS)
+library(readxl)
+library(imager)
+library(clipr)
+library(readr)
+library(fields)
+
+
